881b754f0255805c784098c8dabf2f56c4c1ed03
Joelios <joel.mesmer@libracore.com>
2020-04-16 11:50:51 +0200

    test1

--- doppelpass/hooks.py
+++ doppelpass/hooks.py
@@ -35,7 +35,7 @@ base_template_path = "/doppelpass/templates/base.html"
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+home_page = "setup"
 
 # website user home page (by Role)
 # role_home_page = {
