2bf851c791ca28a2681724c6f2c7eda7b131a287
Joelios <joel.mesmer@libracore.com>
2020-04-20 16:44:38 +0200

    bugfix

--- doppelpass/www/presenz.py
+++ doppelpass/www/presenz.py
@@ -30,27 +30,27 @@ def get_context(context):
 		for teilnehmer in event.anmeldungen:
 			if frappe.session.user == teilnehmer.user:
 				context["anwesend"] = teilnehmer.name
-				
-		# alle events in zukunft
-		context["events_in_zukunft"] = frappe.db.sql("""SELECT
-														`tabDP Event`.`typ`,
-														`tabDP Event`.`start`,
-														`tabDP Event`.`ort`,
-														`tabDP Event`.`name`
-													FROM `tabDP Event`
-													WHERE DATE(`start`) > '{nowdate}'
-													ORDER BY `start` ASC""".format(nowdate=nowdate()), as_dict=True)
-		context["details_zu_events"] = {}
-		for _event in context["events_in_zukunft"]:
-			_event = frappe.get_doc("DP Event", _event.name)
-			context["details_zu_events"][_event.name] = {}
-			context["details_zu_events"][_event.name]["anwesend"] = False
-			context["details_zu_events"][_event.name]["anzahl"] = len(_event.anmeldungen)
-			for teilnehmer in _event.anmeldungen:
-				if frappe.session.user == teilnehmer.user:
-					context["details_zu_events"][_event.name]["anwesend"] = teilnehmer.name
 	except:
 		context["event"] = None
 		context["anwesend"] = False
+				
+	# alle events in zukunft
+	context["events_in_zukunft"] = frappe.db.sql("""SELECT
+													`tabDP Event`.`typ`,
+													`tabDP Event`.`start`,
+													`tabDP Event`.`ort`,
+													`tabDP Event`.`name`
+												FROM `tabDP Event`
+												WHERE DATE(`start`) > '{nowdate}'
+												ORDER BY `start` ASC""".format(nowdate=nowdate()), as_dict=True)
+	context["details_zu_events"] = {}
+	for _event in context["events_in_zukunft"]:
+		_event = frappe.get_doc("DP Event", _event.name)
+		context["details_zu_events"][_event.name] = {}
+		context["details_zu_events"][_event.name]["anwesend"] = False
+		context["details_zu_events"][_event.name]["anzahl"] = len(_event.anmeldungen)
+		for teilnehmer in _event.anmeldungen:
+			if frappe.session.user == teilnehmer.user:
+				context["details_zu_events"][_event.name]["anwesend"] = teilnehmer.name
 	
 	return context
