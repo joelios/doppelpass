914e8f84be3c59de002b7b0576b4b5ec123088a5
Joelios <joel.mesmer@libracore.com>
2020-04-20 16:56:18 +0200

    bugfix

---
+++ doppelpass/www/submenu_profil.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, msmr.ch and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+
+no_cache = 1
