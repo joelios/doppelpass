36054a968e5731d36805fb97de545f9e5905084a
Joelios <joel.mesmer@libracore.com>
2020-04-22 14:14:37 +0200

    ready for rollout

---
+++ doppelpass/www/profil.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, msmr.ch and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils.data import nowdate
+
+no_cache = 1
+
+def get_context(context):
+	if frappe.session.user=='Guest':
+		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+	
+	context["user"] = frappe.get_doc("DP User", frappe.session.user)
+	
+	return context
