b40b6ea9abf1397dae47c451c09fa907b5720d82
Joelios <joel.mesmer@libracore.com>
2020-04-22 13:53:04 +0200

    removed old files and realign hauptmenu

--- doppelpass/www/alt_submenu_training.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2020, msmr.ch and Contributors
-# MIT License. See license.txt
-
-from __future__ import unicode_literals
-import frappe
-from frappe import _
-
-no_cache = 1
