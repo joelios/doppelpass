39ea751d529cf21714101f888a82c9188f1f1aa4
Joelios <joel.mesmer@libracore.com>
2020-04-22 11:39:04 +0200

    implement modul kasse

---
+++ doppelpass/www/kasse.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, msmr.ch and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils.data import nowdate
+
+no_cache = 1
+
+def get_context(context):
+	if frappe.session.user=='Guest':
+		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+	
+	context["saldo"] = frappe.db.get_single_value('DP Kasse', 'saldo')
+	context["bezahlt"] = frappe.db.get_single_value('DP Kasse', 'bezahlt')
+	context["offen"] = frappe.db.get_single_value('DP Kasse', 'offen')
+	context["bussen"] = frappe.db.sql("""SELECT
+											`tabDP Bussen`.`betrag`,
+											`tabDP User`.`fullname`
+										FROM `tabDP Bussen`
+										INNER JOIN `tabDP User`
+										ON `tabDP Bussen`.`user` = `tabDP User`.`name`""", as_dict=True)
+	
+	return context
