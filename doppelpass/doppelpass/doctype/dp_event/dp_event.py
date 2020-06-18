1cab609c51f74805aef05ac41337dbf7bf37371b
Joelios <joel.mesmer@libracore.com>
2020-06-18 09:32:18 +0200

    #17 & Release V4.0.0

--- doppelpass/doppelpass/doctype/dp_event/dp_event.py
+++ doppelpass/doppelpass/doctype/dp_event/dp_event.py
@@ -3,8 +3,33 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class DPEvent(Document):
-	pass
+	def validate(self):
+		if not self.anmeldungen:
+			users = frappe.db.sql("""SELECT `name`, `fullname` FROM `tabDP User`""", as_dict=True)
+			for user in users:
+				row = self.append('anmeldungen', {})
+				row.user = user.name
+				row.fullname = user.fullname
+				
+@frappe.whitelist()
+def add_spieler(spieler, fullname):
+	try:
+		events = frappe.db.sql("""SELECT `name` FROM `tabDP Event` WHERE `start` >= NOW()""", as_dict=True)
+		for _event in events:
+			event = frappe.get_doc("DP Event", _event.name)
+			found = False
+			for _spieler in event.anmeldungen:
+				if _spieler == spieler:
+					found = True
+			if not found:
+				row = event.append('anmeldungen', {})
+				row.user = spieler
+				row.fullname = fullname
+				event.save()
+		return 'ok'
+	except Exception as e:
+		return e
--- doppelpass/doppelpass/doctype/dp_user/dp_user.js
+++ doppelpass/doppelpass/doctype/dp_user/dp_user.js
@@ -2,7 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('DP User', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__("Spieler zu allen künftigen Events hinzufügen"), function() {
+            hinzufuegen(frm);
+        });
+	}
 });
+
+
+function hinzufuegen(frm) {
+	frappe.call({
+       method: "doppelpass.doppelpass.doctype.dp_event.dp_event.add_spieler",
+       args: {
+            "spieler": frm.doc.name,
+            "fullname": frm.doc.fullname
+       },
+       callback: function(response) {
+            var antwort = response.message;
+            if (antwort) {
+               if (antwort == 'ok') {
+				   frappe.msgprint("Der Spieler wurde zu allen Events hinzugefügt.");
+			   } else {
+				   frappe.msgprint(String(antwort));
+			   }
+            } else {
+               frappe.msgprint("error");
+            }
+       }
+    });
+}
